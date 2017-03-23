682c5d7c24e0b38f2dbd2b07c4ddec11d0bf143d
Caio Kinzel Filho <caiokf@gmail.com>
2017-03-23 15:27:19 +0200

    redux

--- client/router.js
+++ client/router.js
@@ -1,28 +1,33 @@
-import React from "react";
-import { Router, Route, browserHistory, IndexRoute } from "react-router";
+import React from 'react';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
 
-// Layouts
-import MainLayout from "./layouts/main.layout";
+import MainLayout from './layouts/main.layout';
+import NotFound from './pages/not-found';
+import Home from './pages/index';
+import Error from './pages/error';
 
-// Pages
-import NotFound from "./pages/not-found";
-import Home from "./pages/index";
-import Error from "./pages/error";
+import store from './store';
+
+const history = syncHistoryWithStore(browserHistory, store);
 
 export default (
-  <Router history={browserHistory}>
+  <Provider store={store}>
+    <Router history={history}>
 
-    <Route component={MainLayout}>
-      <Route path="/" component={Home} />
-    </Route>
+      <Route component={MainLayout}>
+        <Route path="/" component={Home} />
+      </Route>
 
-    <Route component={MainLayout}>
-      <Route path="/error" component={Error} />
-    </Route>
+      <Route component={MainLayout}>
+        <Route path="/error" component={Error} />
+      </Route>
 
-    <Route component={MainLayout}>
-      <Route path="*" component={NotFound} />
-    </Route>
+      <Route component={MainLayout}>
+        <Route path="*" component={NotFound} />
+      </Route>
 
-  </Router>
+    </Router>
+  </Provider>
 );
